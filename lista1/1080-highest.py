efae1befdbef0e4047ded541622bf56e12c4b12f
ni-ela <feitosaferreiradaniela@gmail.com>
2020-08-29 13:20:56 -0400

    feat: atualizacao da questao 1080 e 1160

--- lista1/1080-highest.py
+++ lista1/1080-highest.py
@@ -1,16 +1,29 @@
-# ler 100 inteiros e imprimir o maior valor e sua posicao
-list = []
+# Exercicio 1080: ler 100 inteiros e imprimir o maior valor e sua posicao
+
+maior = 0
+pos = 0
 
 # percorre de 1 a 100 e salva os numeros
 for i in range(0, 100):
     n = int(input())
-    list.append(n)
 
-# verifica o maior
-maior=max(list)
 
-#imprime-o e seu indice
-print(maior,list.index(maior)+1, sep='\n')
+    # verifica o maior comparando entrada atual com anterior
+    maior = n if n > maior else maior
+    pos = i if maior == n else pos
+
+print(maior, pos)
+
+
+
+
+## Outra forma usando lista:
+
+# maior=max(list)
+
+## imprime-o e seu indice
+
+# print(maior,list.index(maior)+1, sep='\n')
 
 
 
--- lista1/1160-increasepop.py
+++ lista1/1160-increasepop.py
@@ -1,33 +1,39 @@
-# Exercicio: imprimir, para cada caso de teste, quantos anos seriam necessários para que a cidade A se tornasse maior que a cidade B (em habitantes). 
-
-Preste atenção, por favor: A população é sempre um número inteiro. Portanto, um crescimento d
-e 2,5% sobre uma população de 100 resultará em 102 (em vez de 102,5) e um crescimento de 2,5% sobre
- uma população de 1000 resultará em 1025. Além disso, use variáveis ​​duplas para a taxa de crescimento.
-
-Resultado
-Lembre-se que se este tempo for maior que 100 será necessário 
-imprimir a mensagem: "Mais de 1 segundo". Em cada um desses casos, talvez seja interessante interromper 
-a contagem, caso contrário você obterá "Limite de tempo excedido".
-
-
+# Exercicio 1160: imprimir, para cada caso de teste, quantos anos seriam necessários para que a cidade A se tornasse maior que a cidade B (em habitantes). 
 
 # recebe qtde de casos de teste
 t = int(input())
 
-# anos
+# listagem dos anos
 anos = []
 
+# contador de anos
+tempo = 0
+
+# salva os casos de teste
 for i in range (t):
-    pa = int(input())
-    pb = int(input())
+
+    # qtde da populacao e respectivos crescimentos populacionais (%)
+    pa, pb, g1, g2 = map(float, input().split())
+
+    # o laco atualiza os valores ate pa alcancar pb
+    while pa <= pb:
+
+        # reutiliza as variaveis pa e pb de modo a transforma-las em taxas de crescimento
+        pa = int((1+g1/100)*pa)
+        pb = int((1+g2/100)*pb)
+        tempo += 1 # enquanto a condicao nao é satisfeita, incrementa-se 1 ano
+
+        # verfifica e aplica break para evitar tempo limite excedido 
+        if tempo > 100:
+            break
+
+    # listagem recebe o resultado do caso de teste i (atual),
+    # if tempo é nao ultrapassa 100 anos, a qtde de anos sera exibida
+    # caso contrario, 'Mais de 1 seculo' sera registrado
+    anos.append(str(tempo) + ' anos.' if tempo <= 100 else 'Mais de 1 seculo.')
     
-    #respectivos crescimentos populacionais (%)
-    g1 = int(input())
-    g2 = int(input()) # com um dígito após a vírgula
+    # reinicia contador
+    tempo = 0
 
-    cresca = pa * g1 + pa
-    crescb = pb * g2 + pa
-
-    cres
-    print('%.0f' % cresc)
-    anos.append()
+# imprime a analise de cada caso
+print(*anos, sep='\n')
